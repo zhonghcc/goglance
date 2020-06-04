fb4a8b6a8c6dcf3d19e93a20140ca522035bb8a5
zhonghcc <zhonghcc@gmail.com>
2020-06-04 12:52:20 +0800

    operator bit zero

--- src/test/operator/operator_test.go
+++ src/test/operator/operator_test.go
@@ -9,3 +9,11 @@ func TestString(t *testing.T) {
 	var b string = "b"
 	t.Log(a + b)
 }
+
+func TestBitZero(t *testing.T) {
+	a := 7
+	check := 0x2
+	t.Log(a &^ check)
+	check = 6
+	t.Log(a &^ check)
+}
