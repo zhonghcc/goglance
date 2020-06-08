b2e5c8b3e34a49a2a3c820068a43dce73c3221d9
zhonghcc <zhonghcc@gmail.com>
2020-06-08 12:12:36 +0800

    array test

--- src/test/array/array_test.go
+++ src/test/array/array_test.go
@@ -1,8 +1,31 @@
 package array
 
-func TestInit(t *testing.T){
-	a := [...]int{1,2,3}
-	b := [3]int
+import "testing"
+
+func TestInit(t *testing.T) {
+	a := [...]int{1, 2, 3}
+	b := [3]int{}
 	t.Log(a)
 	t.Log(b)
+	b[1] = 2
+	t.Log(b)
+}
+
+func TestCompre(t *testing.T) {
+	a := [...]int{1, 2, 3}
+	// b := [...]int{1, 2, 3, 4}
+	c := [...]int{1, 2, 4}
+	d := [...]int{1, 2, 3}
+	// t.Log(a == b)
+	t.Log(a == c)
+	t.Log(a == d)
+
+}
+
+func TestSub(t *testing.T) {
+	a := [...]int{1, 2, 3}
+	t.Log(a[0:1])
+	t.Log(a[0:len(a)])
+	// t.Log(a[0:-1])
+
 }
