e2bf4ea538e53cf7829c21de56d2080bc69c718c
zhonghcc <zhonghcc@gmail.com>
2020-06-09 13:05:01 +0800

    rune

--- src/test/string/string_test.go
+++ src/test/string/string_test.go
@@ -4,6 +4,7 @@ import (
 	"strconv"
 	"strings"
 	"testing"
+	"unicode/utf8"
 )
 
 func TestStringInit(t *testing.T) {
@@ -27,4 +28,18 @@ func TestStringFunc(t *testing.T) {
 	} else {
 		t.Error(err)
 	}
+}
+
+func TestCodec(t *testing.T) {
+	a := "hello"
+	b := "你好"
+	t.Logf("%s,len=%d,rune=%d", a, len(a), len([]rune(a)))
+	t.Logf("%s,len=%d,rune=%d", b, len(b), len([]rune(b)))
+	t.Log(utf8.RuneCountInString(b))
+
+	c := []byte(b)[3:6]
+	t.Log(string(c))
+
+	d := []rune(b)[1]
+	t.Log(string(d))
 }
