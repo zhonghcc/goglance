68469b44d9f48419c138a51927ba741ca9d5e358
zhonghcc <zhonghcc@gmail.com>
2020-06-15 13:01:13 +0800

    nested struct

--- src/test/oop/extension_test.go
+++ src/test/oop/extension_test.go
@@ -12,6 +12,9 @@ type Pet struct {
 func (*Pet) speak() {
 	fmt.Println("...")
 }
+func (p *Pet) getColor() int {
+	return p.color
+}
 
 type Dog struct {
 }
@@ -23,4 +26,38 @@ func (*Dog) speak() {
 func TestExtension(t *testing.T) {
 	var dog *Dog = new(Dog)
 	dog.speak()
+	var pet *Pet = new(Pet)
+	pet.speak()
+	// var pet2 *Pet = dog
+	// pet2.speak()
+
+}
+
+type DogWithParent struct {
+	parent *Pet
+}
+
+func (this *DogWithParent) speak() {
+	this.parent.speak()
+}
+
+func TestExtensionParent(t *testing.T) {
+	var dog *DogWithParent = new(DogWithParent)
+	dog.speak()
+}
+
+type DogWithStruct struct {
+	Pet
+}
+
+//重写了speak，如果不重写，实际会调用Pet对象的speak
+func (this *DogWithStruct) speak() {
+	fmt.Println("wangwangwang")
+}
+func TestExtensionStruct(t *testing.T) {
+	var dog *DogWithStruct = new(DogWithStruct)
+	dog.speak()
+	dog.color = 4
+	t.Log(dog.getColor())
+	t.Log(dog.Pet.getColor())
 }
